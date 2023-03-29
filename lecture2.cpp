56268e91a5ffca3b6b4dcc3e96000f60f6310953
Srijan Tripathi <srijantripathi01@gmail.com>
2023-03-29 10:29:16 +0530

    Covered basic variables in cpp

---
+++ lecture2.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+   cout<<"Namaste duniya"<<endl;
+   int a=4;
+   long int l=123;   
+   long long int ll=123456;
+   bool b= true;
+   char c='S';
+   float f=1.2;
+   double d=1.23;   
+   long double ld=1.23235;
+   
+   cout<<a<<endl;
+   cout<<l<<endl;
+   cout<<ll<<endl;
+   cout<<b<<endl;
+   cout<<c<<endl;
+   cout<<f<<endl;
+   cout<<d<<endl;   
+   cout<<ld<<endl;
+   
+   
+   
+   cout<<"Int size "<<sizeof(int)<<endl;
+   cout<<"bool size "<<sizeof(bool)<<endl;
+   cout<<"float size "<<sizeof(float)<<endl;
+   cout<<"double size "<<sizeof(double)<<endl;
+   cout<<"char size "<<sizeof(char)<<endl;
+   cout<<"long int size "<<sizeof(long int)<<endl;
+   cout<<"long long int size "<<sizeof(long long int)<<endl;
+   cout<<"long double size "<<sizeof(long double)<<endl;
+   
+   
+   
+    
+   return 0;
+}
